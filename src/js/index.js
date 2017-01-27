07d944234cc9231ad62f41a1bb4657071202c1b0
Ksu <ksuwa2711@gmail.com>
2017-01-27 10:20:16 +0300

    Fix scroll

--- src/js/index.js
+++ src/js/index.js
@@ -1,81 +1,34 @@
 $(document).ready(function () {
 //Animation content on main-page
-    $('#main-h1').addClass("show");
-    $('.main-page__tech').addClass("show");
-    $('.animate-mouse').addClass("show");
+  $('#main-h1').addClass("show");
+  $('.main-page__tech').addClass("show");
+  $('.animate-mouse').addClass("show");
 
 //Show/hide menu
-    $('.header__menu').click(function () {
-        $(this).toggleClass('open');
-        $('.nav').toggleClass('show');
-    });
-
-//Mouse icon onclick move to next page
-    var mouse = document.querySelector('.animate-mouse');
-    mouse.addEventListener('click', function () {
-        $('.main-page').removeClass('active').addClass('hide-up');
-        $('.wrapper-show-sections').removeClass('hide-down').addClass('active');
-    });
-//Scroll to the next/previous page
-
-    var sectionsWraper = $('.page-wrapper');
-    var section = 'section';
-    var sectionLast = $('section:last-child');
-    var isScrolling = false;
-
-    var nextSection = function () {
-
-        // if (isScrolling) {
-        //     return;
-        // }
-        //
-        // isScrolling = true;
-        //
-        // setTimeout(function () {
-        //     isScrolling = false;
-        // }, 1000);
-        // $('.main-page').addClass('hide-up').removeClass('active');
-        // $('.wrapper-show-sections').removeClass('hide-down').addClass('active');
-    };
+  $('.header__menu').click(function () {
+    $(this).toggleClass('open');
+    $('.nav').toggleClass('show');
+  });
 
 
-    function previousSection() {
-        // if (isScrolling) {
-        //     return;
-        // }
-        //
-        // isScrolling = true;
-        //
-        // setTimeout(function () {
-        //     isScrolling = false;
-        // }, 1000);
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
 
-        //     $('.wrapper-show-sections')
-        //         .addClass('hide-down')
-        //         .removeClass('active');
-        //
-        // $('.main-page').addClass('active').removeClass('hide-up');
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      event.preventDefault();
 
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
     }
-
-    // $(window).bind('mousewheel', function (event) {
-    //     var delta = event.originalEvent.wheelDelta || -event.originalEvent.detail;
-    //     console.log(delta);
-    //     var target = event.target;
-    //     if (delta > 0) {
-    //         // upscroll code
-    //         if (target.targetName = $('div.wrapper-show-sections')){
-    //         // previousSection();
-    //         }
-    //         console.log('up');
-    //
-    //     } else {
-    //         console.log(sectionLast);
-    //         if (target.targetName = $('section.main-page')) {
-    //             nextSection();
-    //         }
-    //         console.log('down');
-    //     }
-    // });
+  });
 });
-
